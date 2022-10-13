4bb9a4783e8940739f4ecfc7fc1fb05daa2f102f
khd <kwonhyeokdo1209@gmail.com>
2022-10-13 09:39:34 +0900

    이클립스 커밋 실험

--- src/main/java/com/toyproject/dotudy/sign/SignupController.java
+++ src/main/java/com/toyproject/dotudy/sign/SignupController.java
@@ -2,16 +2,18 @@ package com.toyproject.dotudy.sign;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toyproject.dotudy.auth.phone.service.PhoneAuthService;
 
 @RestController
+@RequestMapping("/signup")
 public class SignupController {
 	@Autowired
 	PhoneAuthService phoneAuthService;
 	
-	@GetMapping("/signup/phone")
+	@GetMapping("/auth-number/수정수정")
 	public String phone() {
 		return phoneAuthService.getAuthNumber();
 	}
